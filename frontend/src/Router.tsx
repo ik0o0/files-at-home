3b31837b23ac3e188d9b96bac11f993e043c00c9
ik0o0 <noeh08000@gmail.com>
2025-01-16 05:54:54 +0100

    Added router and soft config.

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,8 +1,18 @@
+import { HelmetProvider } from "react-helmet-async"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { NotFound } from "./pages/NotFound"
+
 function Router() {
   return (
-    <>
-    
-    </>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   )
 }
 
