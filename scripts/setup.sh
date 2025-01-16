a3e066dc263d404339a25448f0858f55d3beec9c
ik0o0 <noeh08000@gmail.com>
2025-01-16 06:37:27 +0100

    Created setup.sh

---
+++ scripts/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Run with root (sudo) privileges."
+    exit 1
+fi
+
+echo "Installing system dependencies..."
+sudo apt update
+sudo apt install -y nodejs npm apache2 mysql-server
+
+echo "Server configuration..."
+./scripts/config-sever.sh
+
+echo "Backend installation..."
+./scripts/install-backend.sh
+
+echo "Frontend installation..."
+./scripts/install-frontend.sh
+
+echo "Configuration is done. You can access it at url:PORT on your LAN."
