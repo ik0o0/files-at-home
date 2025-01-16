6db452714043a5ae67f4bb37e23f7602a2eb1ded
ik0o0 <noeh08000@gmail.com>
2025-01-16 07:36:30 +0100

    Updated setup.sh + UX + logical.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,21 +1,52 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+RESET='\033[0m'
+
 if [ "$EUID" -ne 0 ]; then
-    echo "Run with root (sudo) privileges."
+    echo -e "${RED}Run with root (sudo) privileges.${RESET}"
     exit 1
 fi
 
-echo "Installing system dependencies..."
+echo -e "\n${CYAN}======================================================"
+echo -e "\t⚛️  Welcome to FilesAtHome installer ⚛️"
+echo -e "======================================================${RESET}\n"
+read -p "Are you sure you want to continue the installation? Y/n " RESPONSE
+
+if [ "$RESPONSE" = "n" ] || [ "$RESPONSE" = "N" ]; then
+    echo -e "Cancellation..."
+    exit 1
+fi
+
+echo -e "\n${BLUE}======================================================"
+echo -e "\tInstalling system requirements..."
+echo -e "======================================================${RESET}\n"
 sudo apt update
 sudo apt install -y nodejs npm apache2 mysql-server
 
-echo "Server configuration..."
-./scripts/config-server.sh
+echo -e "\n${BLUE}======================================================"
+echo -e "\tServer configuration..."
+echo -e "======================================================${RESET}\n"
+chmod +x config-server.sh
+sudo ./config-server.sh
 
-echo "Backend installation..."
-./scripts/install-backend.sh
+echo -e "\n${BLUE}======================================================"
+echo -e "\tBackend installation..."
+echo -e "======================================================${RESET}\n"
+chmod +x install-backend.sh
+sudo ./install-backend.sh
 
-echo "Frontend installation..."
-./scripts/install-frontend.sh
+echo -e "\n${BLUE}======================================================"
+echo -e "\tFrontend installation..."
+echo -e "======================================================${RESET}\n"
+chmod +x install-frontend.sh
+sudo ./install-frontend.sh
 
-echo "Configuration is done. You can access it at url:PORT on your LAN."
+echo -e "\n${GREEN}======================================================"
+echo -e "\tConfiguration is done."
+echo -e "\tYou can access it at url:PORT on your LAN."
+echo -e "======================================================${RESET}"
